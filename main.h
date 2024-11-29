8aeb15c799c9598bbad9df5e3b6b7a751e8d8a75
ichnajj <10025@holbertonstudents.com>
2024-11-29 10:20:11 +0100

    Printf project

--- main.h
+++ main.h
@@ -5,7 +5,9 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+
 int _putchar(char c);
+
 int print_c(va_list args);
 int print_s(va_list args);
 int print_int(va_list args);
@@ -14,8 +16,9 @@ int print_number(int n);
 int print_int_helper(int n);
 
 typedef struct specifier {
-    char spec;
-    int (*func)(va_list args);
+	char spec;
+
+	int (*func)(va_list args);
 } specifier_t;
 
-#endif 
+#endif
