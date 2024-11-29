1e9b5042a1fb7e6289c81eef123215bc5c371d38
ichnajj <10025@holbertonstudents.com>
2024-11-29 10:31:08 +0100

    Printf project

--- 0_printf.c
+++ 0_printf.c
@@ -21,6 +21,9 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i = 0, len = 0;
 
+if (format == NULL) {
+		return (-1);
+	}
 	va_start(args, format);
 
 	while (format && format[i])
@@ -51,7 +54,4 @@ int _printf(const char *format, ...)
 
 	va_end(args);
 	return (len);
-	if (format == NULL) {
-		return (-1);
-	}
 }
