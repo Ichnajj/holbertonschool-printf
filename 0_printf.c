59cb3658be1d5f9fa33c597f639b845466e8f9c1
ichnajj <10025@holbertonstudents.com>
2024-11-29 11:10:31 +0100

    Printf project

--- 0_printf.c
+++ 0_printf.c
@@ -21,6 +21,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
+		if (format[i] == '%' && *(format [i+ 1] ) == '\0')
+		return (-1);
 		 if (format[i] == '%')
 		{
 			if (format[i + 1] == '%')
